3327172f96b21683007718ac369b13bc3c363d12
Ricky Andrianto K <ricky.kusuma@student.umn.ac.id>
2022-07-12 13:09:38 +0700

    Fix: Domain for images

--- next.config.js
+++ next.config.js
@@ -1,7 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: ["localhost", "https://rickykusuma.vercel.app/"],
+  images: {
+    domains: ["localhost", "rickykusuma.vercel.app"],
+  },
 };
 
 module.exports = nextConfig;
